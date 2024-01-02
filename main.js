d4373f217aeefd1129095c9bd2cd4e7c9fa4b504
nandhamukundhan <92673956+nandhamukundhan@users.noreply.github.com>
2024-01-02 14:06:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+song1 = "";
+song2 = "";
+
+function preload()
+{
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
+}
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+}
+b 
